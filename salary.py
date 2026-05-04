700a99ad36d3cf612aa202e3dbd3f14709906ea2
Aferokript <alberto.cardinale@bk.ru>
2026-05-04 13:37:48 +0300

    Update salary.py

--- salary.py
+++ salary.py
@@ -1,46 +1,6 @@
-import requests
-
-HABR_URL = 'https://career.habr.com/api/frontend/vacancies'
-
-
-def get_work():
-    params = {
-        'q': 'python',
-    }
-    response = requests.get(HABR_URL, params=params)
-    response.raise_for_status()
-    data = response.json()
-    return data
-
-
-def predict_rub_salary(vacancies):
-
-    list_of_vacancies = []
-    for vacancy in vacancies:
-        salary = vacancy.get('salary')
-
-        if not salary:
-            continue
-
-        if salary.get('from') and salary.get('to'):
-            predicted = (salary['from'] + salary['to']) / 2
-
-        elif salary.get('from'):
-            predicted =  salary['from'] * 1.2
-
-        elif salary.get('to'):
-            predicted = salary['to'] * 0.8
-
-        else:
-            continue
-
-        list_of_vacancies.append(predicted)
-    return list_of_vacancies
-
-
 def get_salary(work):
     languages = ['Python', 'Java', 'JavaScript', 'Ruby', 'PHP', 'C++', 'C#', 'Go']
-
+    url = 'https://career.habr.com/api/frontend/vacancies'
     result = {}
 
     for lang in languages:
@@ -52,6 +12,18 @@ def get_salary(work):
         vacancies_found = vacancies['meta']['totalResults']
         vacancies_processed = predict_rub_salary(vacancies_list)
         average_salary = sum(vacancies_processed) / len(vacancies_processed) if vacancies_processed else 0
+
+        page = 0
+        pages_number = 1
+
+        while page < pages_number:
+            page_response = requests.get(url, params={'page': pages_number})
+            page_response.raise_for_status()
+
+            page_payload = page_response.json()
+            page_payload = page_payload['list']
+            page += 1
+
         result[lang] = {
             "vacancies_found": vacancies_found,
             "vacancies_processed": vacancies_processed,
@@ -59,19 +31,10 @@ def get_salary(work):
         }
         return result
 
+work = get_work()
+salary = get_salary(work)
+print(salary)
 
-def download_pages(pages):
-    url = 'https://career.habr.com/api/frontend/vacancies'
-    page = 0
-    pages_number = 1
-
-    while page < pages_number:
-        page_response = requests.get(url, params={'page': pages_number})
-        page_response.raise_for_status()
-
-        page_payload = page_response.json()
-        page_payload = page_payload['list']
-        page += 1
 
 
 
