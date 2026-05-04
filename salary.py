14b4d4bbd4174c5505ef271670c42c6fb0ab5711
Aferokript <alberto.cardinale@bk.ru>
2026-05-04 13:56:16 +0300

    Update salary.py

--- salary.py
+++ salary.py
@@ -10,30 +10,32 @@ def get_salary(work):
         vacancies = response.json()
         vacancies_list = vacancies['list']
         vacancies_found = vacancies['meta']['totalResults']
-        vacancies_processed = predict_rub_salary(vacancies_list)
-        average_salary = sum(vacancies_processed) / len(vacancies_processed) if vacancies_processed else 0
+        salaries = predict_rub_salary(vacancies_list)
+        processed = len(salaries)
+        average_salary = sum(salaries) / processed if processed else 0
 
+        vacancies_list = []
         page = 0
-        pages_number = 1
+        pages_number = vacancies['meta']['totalPages']
 
         while page < pages_number:
-            page_response = requests.get(url, params={'page': pages_number})
+            page_response = requests.get(url, params={'page': page})
             page_response.raise_for_status()
-
             page_payload = page_response.json()
             page_payload = page_payload['list']
+            vacancies_list.extend(page_payload)
+
             page += 1
 
         result[lang] = {
             "vacancies_found": vacancies_found,
-            "vacancies_processed": vacancies_processed,
+            "vacancies_processed": salaries,
             "average_salary": average_salary
         }
-        return result
+    return result
+
 
 work = get_work()
-salary = get_salary(work)
-print(salary)
 
 
 
